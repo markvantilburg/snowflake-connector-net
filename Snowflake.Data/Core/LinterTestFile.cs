6565142176d5b083400e66ccd8ea389d4ca2784f
Steven Lizano <steven.lizano@snowflake.com>
2024-05-20 13:48:47 -0600

    test commit new linter action

---
+++ Snowflake.Data/Core/LinterTestFile.cs
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+ */
+
+using System.Text;
+using System.Collections.Generic;
+using Google.Cloud.Storage.V1;
+using System;
+
+namespace Snowflake.Data.Core
+{
+    public  class LinterTestFile
+    {
+        internal  ResultFormat ResultFormat { get; }
+
+        public int RowCount { get; protected set; }
+
+        public int ColumnCount { get; protected set; }
+
+        public int ChunkIndex { get; protected set; }
+
+        internal int CompressedSize;
+
+        internal int UncompressedSize;
+
+        internal string Url { get; set; }
+
+        internal string[,] RowSet { get; set; }
+
+        public int GetRowCount() => RowCount;
+
+        public int GetChunkIndex() => ChunkIndex;
+
+          internal virtual void ResdDset(ExecResponseChunk chunkInfo, int chunkIndex)
+        {
+            RowCount = chunkInfo.rowCount;
+            Url = chunkInfo.url;
+            ChunkIndex = chunkIndex;
+            CompressedSize = chunkInfo.compressedSize;
+            UncompressedSize = chunkInfo.uncompressedSize;
+        }
+
+        internal virtual void ResetForRetry()
+        {
+        }
+
+        public void something()
+        {
+        }
+    }
+}
