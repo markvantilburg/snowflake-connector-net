272ce15742f1f097bace76d0269cb7f76c0c6c0c
Steven Lizano <steven.lizano@snowflake.com>
2024-05-21 15:10:42 -0600

    use github action for changed files

--- Snowflake.Data/Core/badInterface.cs
+++ Snowflake.Data/Core/badInterface.cs
@@ -3,5 +3,8 @@ namespace Snowflake.Data.Core
     public interface badInterface
     {
         void methodwithnocaps();
+
+        void
+            reallyunnecessarylkongandaijhafkjdhafkjlahsdkjgfhasdjkghkjasdfghkjadshgkjdshgkjasdhgkjdsdhjkhdhhjadhjkfhdkjafhkjdshfkjasdhfkjasdhfkdhafahskljf(int algo);
     }
 }
