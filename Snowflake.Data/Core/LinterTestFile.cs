a366fd96207190de3b57d2cb3913785ba001e1ac
Steven Lizano <steven.lizano@snowflake.com>
2024-05-28 15:26:33 -0600

    added private static field with no s_

--- Snowflake.Data/Core/LinterTestFile.cs
+++ Snowflake.Data/Core/LinterTestFile.cs
@@ -11,39 +11,6 @@ namespace Snowflake.Data.Core
 {
     public  class LinterTestFile
     {
-        internal  ResultFormat ResultFormat { get; }
-        private int PRIVATEVSAR;
-
-        public int RowCount { get; protected set; }
-
-        public int ColumnCount { get; protected set; }
-
-        public int ChunkIndex { get; protected set; }
-
-        internal int CompressedSize;
-
-        internal int UncompressedSize;
-
-        internal string Url { get; set; }
-
-        internal string[,] RowSet { get; set; }
-
-        public int GetRowCount() => RowCount;
-
-        public int GetChunkIndex() => ChunkIndex;
-
-          internal virtual void ResdDset(ExecResponseChunk chunkInfo, int chunkIndex)
-        {
-            RowCount = chunkInfo.rowCount; Url = chunkInfo.url; ChunkIndex = chunkIndex; CompressedSize = chunkInfo.compressedSize; UncompressedSize = chunkInfo.uncompressedSize;
-        }
-
-        internal virtual void ResetForRetry()
-        {
-        }
-
-        public void something()
-        {
-            PRIVATEVSAR = 2;
-        }
+        private static int nuevoCampo;
     }
 }
